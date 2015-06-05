23419c94456041ba437cdc375b2be71aa3e11957
Morley, Jonathan <jmorley@cvent.com>
2015-06-05 14:51:17 -0400

    drop version back to one less likely to collide

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache v2.0"
 description      "Configures sysfs parameters"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.0.99"
 supports         "ubuntu"
 supports         "debian"
 supports         "centos"
