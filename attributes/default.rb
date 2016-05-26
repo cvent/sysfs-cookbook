def4ecce1db627633fc77ac9235657652f943d04
Jonathan Morley <morley.jonathan@gmail.com>
2016-05-26 15:05:37 -0400

    Support centos 6.7 and future updates

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,17 +2,15 @@ default['sysfs']['package'] = 'sysfsutils'
 default['sysfs']['service'] = 'sysfsutils'
 
 default['sysfs']['init_type'] = value_for_platform(
-  %w(redhat centos) => {
-    %w(6.0 6.1 6.2 6.3 6.4 6.5 6.6) => 'sysv',
-    'default' => 'systemd'
+  %w(redhat centos): {
+    default: (node['platform_version'].to_f >= 7 ? 'systemd' : 'sysv')
   },
-  'default' => nil
+  default: nil
 )
 
 default['sysfs']['disable_tuned'] = value_for_platform(
-  %w(redhat centos) => {
-    %w(6.0 6.1 6.2 6.3 6.4 6.5 6.6) => false,
-    'default' => true
+  %w(redhat centos): {
+    default: (node['platform_version'].to_f >= 7)
   },
-  'default' => false
+  default: false
 )
